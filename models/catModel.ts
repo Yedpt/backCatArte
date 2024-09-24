c7a214b866acf11ef6e647962a35b88e6bffb382
AnaMaria-Sole <anamgarcia.a@yahoo.com>
2024-09-24 15:51:17 +0200

    first typescript change from js

--- models/catModel.ts
+++ models/catModel.ts
@@ -1,1 +1,27 @@
 // typescript
+
+import db_connection from "../database/conectionDB.ts";
+import { DataTypes } from "sequelize";
+
+const catModel = db_connection.define(
+    'back_catarte',
+    {
+      // Model attributes are defined here
+      bookTitle: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      authorName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      bookDescription: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+    },
+    {
+     TimeStamp: false
+    },
+  );
+export default catModel;
