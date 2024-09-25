a734a5ea2c9215fc4f7f9115e6ded95af0631663
Omar Lengua <omarns21@gmail.com>
2024-09-25 11:58:07 +0200

    Revert "Changes in the project, installed dependencies, ts implemented"
    This reverts commit 408388b2cbb60db945a64f9a7df35de04f9eac69.

--- app.ts
+++ app.ts
@@ -1,13 +1,1 @@
-import connectionDb from "./database/conectionDB";
-import Book from "./models/catModel";
-
-(async () => {
-  try {
-    await connectionDb.authenticate();
-    console.log('Connection has been established successfully.');
-    await Book.sync({ force: true });
-    console.log('The table for the Book model was just re-created.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+// en typescript
