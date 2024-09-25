77ef6f352566cc31c32f813d968bf245c5c0ed4f
Yeder <ypimentel.tapia@gmail.com>
2024-09-25 13:57:26 +0200

    database configuration

--- database/conectionDB.ts
+++ database/conectionDB.ts
@@ -1,11 +1,20 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
 
-dotenv.config()
+// Simular __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const connectionDb = new Sequelize('catarte', 'root', process.env.DB_PASSWORD , {
-  host: 'localhost',
+// Cargar el archivo .env desde la raíz del proyecto
+dotenv.config({ path: resolve(__dirname, '../.env') }); 
+
+
+const connectionDb = new Sequelize( process.env.DB_NAME || '' , process.env.DB_USER || '' , process.env.DB_PASSWORD || '' , {
+  host: process.env.DB_HOST ,
   dialect: 'mysql',
+  port: Number(process.env.DB_PORT) || 3306,
   define: {
     timestamps: false,
   },
