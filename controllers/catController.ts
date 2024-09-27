9faba8778cd8ecb7bca0caed67cc4fcd602715f4
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-09-27 11:18:49 +0200

    in process funtion delete

--- controllers/catController.ts
+++ controllers/catController.ts
@@ -9,3 +9,19 @@ export const getAllMemes = async (req: Request, res: Response) => {
     res.json({ message: "A ocurrido un error", error });
   }
 };
+
+//funcion para eliminar el meme
+
+export const deleteMeme = async (req: Request, res: Response) => {
+  try {
+    const memeId = req.params.id;
+
+    const meme = await catMeme.findByPk(memeId)
+
+    await meme?.destroy();
+
+  } catch (error) {
+    console.log('El meme no se pudo eliminar', error);
+    
+  }
+};
--- routers/catRouter.ts
+++ routers/catRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getAllMemes } from  '../controllers/catController.js';
+import { deleteMeme, getAllMemes } from  '../controllers/catController.js';
 
 const router = Router();
 
 router.get('/memes', getAllMemes);
+
+router.delete('/:id', deleteMeme);
 
 export default router;
