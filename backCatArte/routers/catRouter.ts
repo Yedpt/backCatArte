2b41e2b59ac7f4199c356e33be662f958af23576
Omar Lengua <omarns21@gmail.com>
2024-10-07 14:41:16 +0200

    feat: update validation logic and add new cat validation

--- backCatArte/routers/catRouter.ts
+++ backCatArte/routers/catRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { deleteMeme, getAllMemes, createMeme, putMeme } from  '../controllers/catController.js';
+import { deleteMeme, getAllMemes, createMeme, putMeme } from '../controllers/catController.js';
+import { validateCreateMeme, validateUpdateMeme, validateDeleteMeme } from '../validations/catValidation.js';
+import { validationHandler } from '../handle/handleValidator.js';
 
 export const router = Router();
 
 // Las rutas relativas, no especificas 'api/memes' aquí prueba
 router.get('/', getAllMemes);
-router.post('/', createMeme);
-router.delete('/:id', deleteMeme);
-router.put('/:id', putMeme);
+router.post('/', validateCreateMeme, validationHandler, createMeme);
+router.delete('/:id', validateDeleteMeme, validationHandler, deleteMeme);
+router.put('/:id', validateUpdateMeme, validationHandler, putMeme);
