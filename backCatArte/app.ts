5693cb96cd5c120668ceba854b0427c0d351ae55
Yeder <ypimentel.tapia@gmail.com>
2024-10-07 15:12:11 +0200

    changin config tsconfig y package json

--- backCatArte/app.ts
+++ backCatArte/app.ts
@@ -10,7 +10,7 @@ import {PORT} from './config';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // el localhost donde esta corriendo el front
+  origin: 'http://localhost:5173', // el localhost donde esta corriendo el frontend
 }));
 app.use(express.json());
 
