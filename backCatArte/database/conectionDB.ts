dc368780d76740cd9cb9cc90e2c59e21aefefb6b
Yeder <ypimentel.tapia@gmail.com>
2024-10-07 11:50:42 +0200

    chaging type on config.ts

--- backCatArte/database/conectionDB.ts
+++ backCatArte/database/conectionDB.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { DB_NAME, DB_USER , DB_PASSWORD , DB_HOST, DB_PORT } from '../config.js';
 
 
-const connectionDb = new Sequelize( DB_NAME || '' , DB_USER || '' , DB_PASSWORD || '' , {
+const connectionDb = new Sequelize( DB_NAME , DB_USER, DB_PASSWORD , {
   host: DB_HOST ,
   dialect: 'mysql',
   port: Number(DB_PORT) || 3306,
