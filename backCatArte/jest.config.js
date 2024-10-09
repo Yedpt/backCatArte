0009bec0ebc43a6bb255d5bf201b5bc93892dda1
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-09 12:34:49 +0200

    settings

--- backCatArte/jest.config.js
+++ backCatArte/jest.config.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
-=======
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: false,
   forceExit: true,
  };
->>>>>>> 862dd143517ead88b06580f8e1fef8f7629586c8
